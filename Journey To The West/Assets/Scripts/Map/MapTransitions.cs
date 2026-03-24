2f90ac548bf1c04837659b007be9ebc3268a4e1a
kxrii <126003983+kxrii@users.noreply.github.com>
2026-03-24 01:04:01 -0400

    Changed code in MapTransition to allow for the player to enter the inside of the dojo

--- Journey To The West/Assets/Scripts/Map/MapTransitions.cs
+++ Journey To The West/Assets/Scripts/Map/MapTransitions.cs
@@ -1,49 +1,61 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using Unity.Cinemachine;
-
 
 public class MapTransitions : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundary;
-    CinemachineConfiner2D confiner;
-
     [SerializeField] Direction direction;
+    [SerializeField] Transform teleportTargetPosition;
 
-    enum Direction { Up, Down, Left, Right };
+    CinemachineConfiner2D confiner;
+    CinemachineCamera vcam; // or CinemachineVirtualCamera if on older Cinemachine
 
-    private void Awake(){
+    enum Direction { Up, Down, Left, Right, Teleport }
+
+    private void Awake()
+    {
         confiner = FindObjectOfType<CinemachineConfiner2D>();
-
+        vcam = FindObjectOfType<CinemachineCamera>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.gameObject.CompareTag("Player")){
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // 1. Update confiner FIRST
             confiner.BoundingShape2D = mapBoundary;
+            confiner.InvalidateBoundingShapeCache();
+
+            // 2. Then move the player
             UpdatePlayerPosition(collision.gameObject);
+
+            // 3. Force the camera to snap to the new position immediately
+            if (direction == Direction.Teleport)
+            {
+                vcam.ForceCameraPosition(
+                    teleportTargetPosition.position + new Vector3(0, 0, -10f),
+                    Quaternion.identity
+                );
+            }
         }
     }
 
     private void UpdatePlayerPosition(GameObject player)
     {
+        if (direction == Direction.Teleport)
+        {
+            player.transform.position = teleportTargetPosition.position;
+            return;
+        }
+
         Vector2 newPos = player.transform.position;
         switch (direction)
         {
-            case Direction.Up:
-                newPos.y += 2;
-                break;
-            case Direction.Down:
-                newPos.y -= 2;
-                break;
-            case Direction.Left:
-                newPos.x -= 2;
-                break;
-            case Direction.Right:
-                newPos.x += 2;
-                break;
+            case Direction.Up:    newPos.y += 2; break;
+            case Direction.Down:  newPos.y -= 2; break;
+            case Direction.Left:  newPos.x -= 2; break;
+            case Direction.Right: newPos.x += 2; break;
         }
-
         player.transform.position = newPos;
     }
-
 }
