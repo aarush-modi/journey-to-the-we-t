59686799cead36696dbe6ea7d9b920561f0e6b59
Aarush Modi <106461131+aarush-modi@users.noreply.github.com>
2026-03-25 03:31:51 -0400

    Added testing files for HUD

---
+++ Journey To The West/Assets/Scripts/Dev/HUDTest.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HUDTest : MonoBehaviour
+{
+    private PlayerCombat playerCombat;
+    private GreedMeter greedMeter;
+
+    private void Awake()
+    {
+        playerCombat = GetComponent<PlayerCombat>();
+        greedMeter = GetComponent<GreedMeter>();
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.hKey.wasPressedThisFrame)
+        {
+            playerCombat.TakeDamage(10f);
+            Debug.Log("HUDTest: took 10 damage");
+        }
+
+        if (Keyboard.current.gKey.wasPressedThisFrame)
+        {
+            greedMeter.AddGold(100);
+            Debug.Log("HUDTest: added 100 gold");
+        }
+    }
+}
