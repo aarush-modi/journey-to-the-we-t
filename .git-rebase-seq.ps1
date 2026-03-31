51cb9ff3a7f6da8d52d7df525a358add1f6ab133
cameron-wongg <cwong969@uwo.ca>
2026-03-31 00:10:57 -0400

    delete unwanted file

--- .git-rebase-seq.ps1
+++
@@ -1,5 +0,0 @@
-param([string]$Path)
-$lines = Get-Content -LiteralPath $Path
-$lines | ForEach-Object {
-    if ($_ -match '^pick ') { $_ -replace '^pick', 'reword' } else { $_ }
-} | Set-Content -LiteralPath $Path
