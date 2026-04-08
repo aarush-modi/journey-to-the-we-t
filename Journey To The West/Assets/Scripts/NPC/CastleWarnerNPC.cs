6492de462ee5d660c326062d29250bc213bd835a
Aarush Modi <106461131+aarush-modi@users.noreply.github.com>
2026-04-08 18:46:52 -0400

    Removed red packet logic from level and NPC

--- Journey To The West/Assets/Scripts/NPC/CastleWarnerNPC.cs
+++ Journey To The West/Assets/Scripts/NPC/CastleWarnerNPC.cs
@@ -8,35 +8,22 @@ public class CastleWarnerNPC : NPCBase
     [SerializeField] private NPCDialogue introDialogue;
     [SerializeField] private NPCDialogue reminderDialogue;
 
-    [Header("Quest & Package")]
-    [SerializeField] private PackageData packageToGive;
-    [SerializeField] private QuestData questToStart;
-
-    private PlayerInventory playerInventory;
-    private bool hasGivenPackage;
+    private bool hasSpoken;
 
     protected override void Start()
     {
-
-
         base.Start();
     }
-    
 
     public override void Interact(GameObject player)
     {
-        if (playerInventory == null)
-            playerInventory = player.GetComponent<PlayerInventory>()
-                ?? player.GetComponentInChildren<PlayerInventory>()
-                ?? player.GetComponentInParent<PlayerInventory>();
-
         if (isDialogueActive)
         {
-            PlayDialogue(hasGivenPackage ? reminderDialogue : introDialogue);
+            PlayDialogue(hasSpoken ? reminderDialogue : introDialogue);
             return;
         }
 
-        if (!hasGivenPackage)
+        if (!hasSpoken)
         {
             OnDialogueComplete.AddListener(OnIntroComplete);
             PlayDialogue(introDialogue);
@@ -50,22 +37,6 @@ public class CastleWarnerNPC : NPCBase
     private void OnIntroComplete()
     {
         OnDialogueComplete.RemoveListener(OnIntroComplete);
-
-        if (hasGivenPackage) return;
-
-        hasGivenPackage = true;
-
-        if (playerInventory != null && packageToGive != null)
-            playerInventory.AddPackage(packageToGive);
-
-        if (questToStart != null)
-            QuestManager.Instance.StartQuest(questToStart);
-    }
-
-    private GameObject MakeRect(string name, Transform parent)
-    {
-        GameObject obj = new GameObject(name, typeof(RectTransform));
-        obj.transform.SetParent(parent, false);
-        return obj;
+        hasSpoken = true;
     }
 }
